1cc5865ff16fb02e9ecc401c778e4ab066e0efe1
s1mpleow <dungdqch@gmail.com>
2024-09-12 15:25:58 +0700

    refactor: Update versions of local and tls providers in dev environment

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -39,24 +39,24 @@ module "SecurityGroups" {
   vpc_cidr_range = module.VPC.vpc_cidr_range
 }
 
-module "ElastiCache" {
-  source      = "../../modules/ElastiCache"
-  redis_sg_id = module.SecurityGroups.redis_sg_id
-  node_type   = "cache.t2.small"
-  tags        = var.default_tags
-  subnet_id   = module.VPC.public_subnet_id_1
-}
+# module "ElastiCache" {
+#   source      = "../../modules/ElastiCache"
+#   redis_sg_id = module.SecurityGroups.redis_sg_id
+#   node_type   = "cache.t2.small"
+#   tags        = var.default_tags
+#   subnet_id   = module.VPC.public_subnet_id_1
+# }
 
-module "RDS" {
-  source = "../../modules/RDS"
-  subnet_ids = [
-    module.VPC.public_subnet_id_1,
-    module.VPC.public_subnet_id_2,
-  ]
-  mysql_password = var.mysql_password
-  tags           = var.default_tags
-  rds_sg_id      = module.SecurityGroups.rds_sg_id
-}
+# module "RDS" {
+#   source = "../../modules/RDS"
+#   subnet_ids = [
+#     module.VPC.public_subnet_id_1,
+#     module.VPC.public_subnet_id_2,
+#   ]
+#   mysql_password = var.mysql_password
+#   tags           = var.default_tags
+#   rds_sg_id      = module.SecurityGroups.rds_sg_id
+# }
 
 # module "DocumentDB" {
 #   source = "../../modules/DocumentDB"
@@ -91,7 +91,7 @@ module "CWLogs" {
   ec2_log_group_name    = "/aws/edx/application"
   log_retention_in_days = 3
   sns_topic_arn         = module.SNS.sns_topic_arn
-  ec2_instance_id       = module.EC2.edx_instance_id
+  ec2_instance_id       = "i-0b1b3b3b3b3b3b3b3"
 }
 
 
@@ -102,48 +102,48 @@ module "IAM" {
   stage_name        = var.stage_name
 }
 
-module "ElasticSearch" {
-  source       = "../../modules/ElasticSearch"
-  domain       = "fastai-es-${var.stage_name}"
-  stage_name   = var.stage_name
-  default_tags = var.default_tags
-  subnet_id    = module.VPC.public_subnet_id_1
-  es_sg_id     = module.SecurityGroups.elasticsearch_sg_id
-}
+# module "ElasticSearch" {
+#   source       = "../../modules/ElasticSearch"
+#   domain       = "fastai-es-${var.stage_name}"
+#   stage_name   = var.stage_name
+#   default_tags = var.default_tags
+#   subnet_id    = module.VPC.public_subnet_id_1
+#   es_sg_id     = module.SecurityGroups.elasticsearch_sg_id
+# }
 
-module "EC2" {
-  source                                 = "../../modules/VirtualMachine"
-  subnet_id_application                  = module.VPC.public_subnet_id_1
-  subnet_id_database                     = module.VPC.public_subnet_id_2
-  edx_sg_id                              = module.SecurityGroups.edx_sg_id
-  mongodb_sg_id                          = module.SecurityGroups.mongodb_sg_id
-  default_tags                           = var.default_tags
-  openedx_ami                            = "ami-022affc20f19fa55d"
-  key_name                               = module.KeyPair.key_name
-  stage_name                             = var.stage_name
-  aws_cloudwatch_agent_profile_role_name = module.IAM.aws_cloudwatch_agent_profile_role_name
-  # MONGODB_HOST                           = module.DocumentDB.documentdb_endpoint
-  # MONGODB_USERNAME                       = module.DocumentDB.documentdb_master_username
-  # MONGODB_PASSWORD                       = module.DocumentDB.documentdb_master_password
-  MONGODB_HOST                           = ""
-  MONGODB_USERNAME                       = ""
-  MONGODB_PASSWORD                       = ""
-  MYSQL_HOST                             = ""
-  MYSQL_ROOT_PASSWORD                    = ""
-  MYSQL_ROOT_USERNAME                    = ""
-  REDIS_HOST                             = ""
-  SMTP_HOST                              = var.SMTP_HOST
-  SMTP_USERNAME                          = var.SMTP_USERNAME
-  SMTP_PASSWORD                          = var.SMTP_PASSWORD
-  ELASTICSEARCH_HOST                     = ""
-  OPENEDX_AWS_SECRET_ACCESS_KEY          = module.IAM.aws_s3_secret_key
-  OPENEDX_AWS_ACCESS_KEY                 = module.IAM.aws_s3_access_key
-  S3_PROFILE_IMAGE_BUCKET                = module.S3.openedx_fastai_profile_images_bucket_name
-  S3_REGION                              = var.region
-  S3_STORAGE_BUCKET                      = module.S3.openedx_fastai_bucket_name
-  mongodb_ami                            = "ami-0d73fbec275a3e034"
-  # depends_on                             = [module.CWLogs, module.IAM, module.RDS, module.ElastiCache, module.VPC, module.SecurityGroups, module.DocumentDB, module.ElasticSearch]
-}
+# module "EC2" {
+#   source                                 = "../../modules/VirtualMachine"
+#   subnet_id_application                  = module.VPC.public_subnet_id_1
+#   subnet_id_database                     = module.VPC.public_subnet_id_2
+#   edx_sg_id                              = module.SecurityGroups.edx_sg_id
+#   mongodb_sg_id                          = module.SecurityGroups.mongodb_sg_id
+#   default_tags                           = var.default_tags
+#   openedx_ami                            = "ami-022affc20f19fa55d"
+#   key_name                               = module.KeyPair.key_name
+#   stage_name                             = var.stage_name
+#   aws_cloudwatch_agent_profile_role_name = module.IAM.aws_cloudwatch_agent_profile_role_name
+#   # MONGODB_HOST                           = module.DocumentDB.documentdb_endpoint
+#   # MONGODB_USERNAME                       = module.DocumentDB.documentdb_master_username
+#   # MONGODB_PASSWORD                       = module.DocumentDB.documentdb_master_password
+#   MONGODB_HOST                           = ""
+#   MONGODB_USERNAME                       = ""
+#   MONGODB_PASSWORD                       = ""
+#   MYSQL_HOST                             = ""
+#   MYSQL_ROOT_PASSWORD                    = ""
+#   MYSQL_ROOT_USERNAME                    = ""
+#   REDIS_HOST                             = ""
+#   SMTP_HOST                              = var.SMTP_HOST
+#   SMTP_USERNAME                          = var.SMTP_USERNAME
+#   SMTP_PASSWORD                          = var.SMTP_PASSWORD
+#   ELASTICSEARCH_HOST                     = ""
+#   OPENEDX_AWS_SECRET_ACCESS_KEY          = module.IAM.aws_s3_secret_key
+#   OPENEDX_AWS_ACCESS_KEY                 = module.IAM.aws_s3_access_key
+#   S3_PROFILE_IMAGE_BUCKET                = module.S3.openedx_fastai_profile_images_bucket_name
+#   S3_REGION                              = var.region
+#   S3_STORAGE_BUCKET                      = module.S3.openedx_fastai_bucket_name
+#   mongodb_ami                            = "ami-0d73fbec275a3e034"
+#   # depends_on                             = [module.CWLogs, module.IAM, module.RDS, module.ElastiCache, module.VPC, module.SecurityGroups, module.DocumentDB, module.ElasticSearch]
+# }
 
 module "S3" {
   source       = "../../modules/S3"
@@ -152,22 +152,22 @@ module "S3" {
   iam_s3_arn   = module.IAM.iam_s3_arn
 }
 
-output "endpoint_redis_edx" {
-  value = module.ElastiCache.endpoint_redis_edx
-}
+# output "endpoint_redis_edx" {
+#   value = module.ElastiCache.endpoint_redis_edx
+# }
 
-output "endpoint_mysql_edx" {
-  value = module.RDS.endpoint_mysql_edx
-}
+# output "endpoint_mysql_edx" {
+#   value = module.RDS.endpoint_mysql_edx
+# }
 
-output "endpoint_es_edx" {
-  value = module.ElasticSearch.es_domain_endpoint
-}
+# output "endpoint_es_edx" {
+#   value = module.ElasticSearch.es_domain_endpoint
+# }
 
-output "mongodb_host" {
-  value = module.EC2.mongodb_host
-}
+# output "mongodb_host" {
+#   value = module.EC2.mongodb_host
+# }
 
-output "edx_public_ip" {
-  value = module.EC2.edx_public_ip
-}
+# output "edx_public_ip" {
+#   value = module.EC2.edx_public_ip
+# }
